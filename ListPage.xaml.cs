e2e47f46763e6993cf522b1b4374adbe2e18667d
marachereches <marachereches@yahoo.com>
2022-12-12 11:58:34 +0200

    lb9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -19,4 +19,27 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+   /* async void OnDeleteProductButtonClicked(object sender, EventArgs e)
+    {
+        var product = (Product)BindingContext;
+        await App.Database.DeleteProductAsync(product);
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+     <Button Grid.Column="1" Text="Delete Product" MaximumWidthRequest="200" Clicked="OnDeleteProductButtonClicked"/>*/
+
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        }); }
+         protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
 }
+  
